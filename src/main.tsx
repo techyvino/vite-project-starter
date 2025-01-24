98a24dc3c409de2659416979e1264d749f722d0f
VINOTHKUMAR S <techyvino@gmail.com>
2025-01-24 13:00:26 +0530

    refactor: update React and ReactDOM imports; switch to ReactDOM.render

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import './index.css';
 
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from './App.tsx';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
